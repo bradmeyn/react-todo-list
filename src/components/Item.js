ff8b75165a0d825499e56b843ce98a7c98f6b40b
Brad Meyn <bradjmeyn@gmail.com>
2021-09-08 17:10:08 +1000

    add delete functionality

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,16 +1,27 @@
-import React from 'react';
-import './Item.css';
+import React, { Component } from 'react'
+import './Item.css'
 
-export const Item = (props) => {
-    return (
-        <div className="item-container">
-            <div className="item">{props.detail}</div>
-            <div className="buttons">
-                <button className="button button-edit">Edit</button>
-                <button className="button button-delete">Delete</button>
+
+export class Item extends Component {
+    constructor(props){
+        super(props)
+        this.handleRemove = this.handleRemove.bind(this);
+    }
+    handleRemove(e){
+        this.props.remove(this.props.detail);
+    }
+    render() {
+        return (
+            <div className="item-container">
+                <div className="item">{this.props.detail}</div>
+                <div className="buttons">
+                    <button className="button button-edit">Edit</button>
+                    <button onClick={this.handleRemove} className="button button-delete">Delete</button>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
+
 
 
--- src/components/List.js
+++ src/components/List.js
@@ -6,18 +6,40 @@ export class List extends Component {
     constructor(props){
         super(props)
         this.state = {
-            items: ["buy groceries", "go to shops", 'workout']
+            items: [
+                {id: 1, detail: 'Purchase groceries'},
+                {id: 2, detail: 'Go to the gym'},
+                {id: 3, detail: 'Walk the dog'}
+            ]
         }
+        this.remove = this.remove.bind(this);
     }
+
+
+    remove(removedItem){
+        console.log(removedItem);
+        this.setState(oldState => ({
+            items: oldState.items.filter((item) => item.detail !== removedItem)
+        }))
+    }
+
+
+
     render() {
+
         return (
             <div className="list">
+                <div className="new-item-container">
+                    <input className="new-item-input" placeholder="Add new item.."></input>
+                    <button className="add" >+</button>
+                </div>
                 <header className="header">
                     {this.props.listName}
                 </header>
                 {this.state.items.map(item => {
-                    return <Item detail={item}/>
+                    return <Item key={item.id} detail={item.detail} remove={this.remove}/>
                 })}
+                
             </div>
         )
     }
